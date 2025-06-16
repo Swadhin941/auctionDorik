72390d0a4782223d15983284bb0608282d159d6f
Swadhin Ghosh <swadhinghosh@Swadhins-MacBook-Air.local>
2025-06-16 09:31:19 +0600

    middleware update

--- routers/auction.product.js
+++ routers/auction.product.js
@@ -1,10 +1,12 @@
 const { express } = require("../config/config");
 const { postProduct } = require("../controller/product");
+const { verifyJWT } = require("../middlewares/verifyJWT");
+const { verifySeller, verifyForbidden } = require("../middlewares/verifyUsers");
 const { registerValidator } = require("../validator /auth.validator");
 const productValidatorSchemas = require("../validator /product.validator.schemas");
 
 const auctionRouters= express.Router();
 
-auctionRouters.post("/upload-product",registerValidator(productValidatorSchemas.postProductSchema), postProduct)
+auctionRouters.post("/upload-product",registerValidator(productValidatorSchemas.postProductSchema),verifyJWT, verifySeller, verifyForbidden, postProduct);
 
 module.exports= auctionRouters;
